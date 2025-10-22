f213730326acee42cb236da95cdefc6265110f3a
krishkumar4400 <krishkumarbgp2022@gmail.com>
2025-10-22 21:35:59 +0530

    students enrolled page

--- Frontend/src/pages/educator/StudentEnrolled.jsx
+++ Frontend/src/pages/educator/StudentEnrolled.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { dummyStudentEnrolled } from '../../assets/assets.js';
 
 const StudentEnrolled = () => {
+  const [studentsEnrolled, setStudentsEnrolled] = useState(null);
+  const fetchStudentsEnrolled = async() => {
+    setStudentsEnrolled(dummyStudentEnrolled);
+  }
+
+  useEffect(() => {
+    fetchStudentsEnrolled();
+  }, [studentsEnrolled]);
+
   return (
     <div>
-      
+      <div>
+        <h1></h1>
+      </div>
     </div>
   )
 }
