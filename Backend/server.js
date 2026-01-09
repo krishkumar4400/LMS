2235a8bbecd96acb663ca5cb10e4a6e47e3da491
krishkumar4400 <krishkumarbgp2022@gmail.com>
2026-01-09 08:10:56 +0530

    backend server of lms project

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,11 @@
+import cookieParser from 'cookie-parser';
 import express from 'express';
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
 
 const port = process.env.PORT || 4000;
 
