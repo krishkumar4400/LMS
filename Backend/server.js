6329d74f89c4ae31df261b1f9432c335d5114f96
krishkumar4400 <krishkumarbgp2022@gmail.com>
2026-01-09 09:23:31 +0530

    user model

--- Backend/server.js
+++ Backend/server.js
@@ -1,18 +1,30 @@
 import cookieParser from 'cookie-parser';
 import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import connectDB from './config/db.js';
 
+// initialize express
 const app = express();
 
+// middlewares
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
+app.use(cors());
 
-const port = process.env.PORT || 4000;
+// connect to database
+await connectDB();
 
+// routes
 app.get('/', (req,res) => {
     res.send("Hello Express"); 
 });
 
+// port 
+const port = process.env.PORT || 4000;
+
+// running server 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`); 
 });
